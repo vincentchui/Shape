2c1dc94bd6e633cd1a4da29edc1a83933bbc69ac
vincentchui <vincentchuicalifornia@gmail.com>
2015-11-01 09:40:28 -0800

    initial

---
+++ Shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Shape
+    {
+        private int shapeID;
+
+        public int ShapeID
+        {
+            get
+            {
+                return shapeID;
+            }
+            set
+            {
+                shapeID = value;
+            }
+        }
+
+        public string ToString()
+        {
+            return;
+        }
+    }
+}
---
+++ ZeroDimensionalShape.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class ZeroDimensionalShape
+    {
+        public class Point
+        {
+            private int cordX;
+            private int cordY;
+            private int cordZ;
+
+            public int CordX
+            {
+                get
+                {
+                    return cordX;
+                }
+                set
+                {
+                    cordX = value;
+                }
+            }
+
+            public int CordY
+            {
+                get
+                {
+                    return CordY;
+                }
+                set
+                {
+                    CordY = value;
+                }
+            }
+
+            public int CordZ
+            {
+                get
+                {
+                    return cordZ;
+                }
+                set
+                {
+                    cordZ = value;
+                }
+            }
+
+            public void setCordX()
+            {
+                Console.Write("\nWhat is the x coordinate? ");
+                CordX = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public void setCordY()
+            {
+                Console.Write("\nWhat is the y coordinate? ");
+                CordY = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public void setCordZ()
+            {
+                Console.Write("\nWhat is the z coordinate? ");
+                CordZ = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public int getCordX()
+            {
+                return CordX;
+            }
+
+            public int getCordY()
+            {
+                return CordY;
+            }
+
+            public int getCordZ()
+            {
+                return CordZ;
+            }
+        }
+    }
+}
